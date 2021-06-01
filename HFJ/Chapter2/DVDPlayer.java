b8901b4eecfc7073f03e36bb5f927277d27f8034
anannyadutta09 <68387132+anannyadutta09@users.noreply.github.com>
2021-06-01 22:50:19 +0530

    Update DVDPlayer.java

--- HFJ/Chapter2/DVDPlayer.java
+++ HFJ/Chapter2/DVDPlayer.java
@@ -1,16 +1,18 @@
 class DVDPlayer {
 	boolean canRecord = false;
 	void recordDVD() {
+		if (canRecord == true) {
 		System.out.println("DVD recording");
+		}
 	}
 }
 class DVDPlayerTestDrive {
 	public static void main(String [] args) {
 		DVDPlayer d = new DVDPlayer();
 		d.canRecord = true;
-		d.PlayDVD();
-		if (d.canRecord == true) {
-			d.recordDVD();
-		} 
+		// d.PlayDVD();
+		 
+		d.recordDVD();
+		 
 	}
 }
