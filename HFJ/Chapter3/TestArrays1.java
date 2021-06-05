bf7599f27d135ed31de29ef703a9355c1f74c713
anannyadutta <anannyadutta09@gmail.com>
2021-06-05 20:33:45 +0530

    Chapter3 part 1

---
+++ HFJ/Chapter3/TestArrays1.java
@@ -0,0 +1,29 @@
+class TestArrays1 {
+	public static void main(String [] args) {
+		String[] islands = new String[4];
+		islands[0] = "Bermuda";
+		islands[1] = "Fiji";
+		islands[2] = "Azores";
+		islands[3] = "Cozumel";
+		int y = 0;
+		while (y < 4) {
+		System.out.print("island = " + islands[y]);
+		y = y + 1;
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HFJ/Chapter3/TestArrays2.java
@@ -0,0 +1,30 @@
+class TestArrays2 {
+	public static void main(String [] args) {
+		String[] islands = new String[4];
+		islands[0] = "Bermuda";
+		islands[1] = "Fiji";
+		islands[2] = "Azores";
+		islands[3] = "Cozumel";
+		int y = 0;
+		while (y < 4) {
+		System.out.print("island = ");
+		System.out.println(islands[y]);
+		y = y + 1;
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
